69437dcc981a1f406922b76ce55690de20e3c69f
James Birtles <james@birtles.dev>
2024-09-18 08:26:11 +0100

    add helper to convert from boolean literals to real booleans

---
+++ .changeset/good-eyes-smell.md
@@ -0,0 +1,5 @@
+---
+'@peopleplus/effect-form': patch
+---
+
+add helper to convert from boolean literals to real booleans
--- src/FormSchema.ts
+++ src/FormSchema.ts
@@ -2,12 +2,15 @@ import { Schema } from '@effect/schema';
 
 export const Date = Schema.Date.annotations({ jsonSchema: { format: 'date' } });
 
+export type BooleanLiteral = typeof Boolean.Encoded;
+export const booleanFromLiteral = (value: BooleanLiteral) => value === 'true' || value === 'on';
+
 interface Boolean extends Schema.Schema<boolean, 'true' | 'false' | 'on' | 'off'> {}
 export const Boolean: Boolean = Schema.transform(
 	Schema.Literal('true', 'false', 'on', 'off'),
 	Schema.Boolean,
 	{
 		encode: (value) => (value ? 'true' : 'false'),
-		decode: (value) => value === 'true' || value === 'on',
+		decode: booleanFromLiteral,
 	},
 ).annotations({ identifier: 'Boolean' });
